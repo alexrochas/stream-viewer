ad8431cbdc7f342a486b7a59168807b910bb4f6e
Alex Rocha <alex.rochas@yahoo.com.br>
2017-02-07 21:06:15 -0200

    feat: add npm package manager

--- src/main/groovy/alex/StreamViewerMain.groovy
+++ src/main/groovy/alex/StreamViewerMain.groovy
@@ -1,18 +1,56 @@
 package alex
 
+import groovy.util.logging.Slf4j
+import spark.Request
+import spark.Response
 import spark.Spark
 
+@Slf4j
 class StreamViewerMain {
 
     public static void main(String[] args) {
         Spark.staticFileLocation("/public") //index.html is served at localhost:4567 (default port)
         Spark.webSocket("/eventbus", EchoWebSocket.class)
+
+        Spark.get("/node/*", {req, res ->
+            String path = req.pathInfo().replaceFirst("/node/", "node_modules/");
+            return serveFile(path,req,res)
+        })
+
         Spark.post("/ping", {req, res ->
             EchoWebSocket.getSessions().stream().filter({s -> s.open}).forEach({ session ->
                 session.getRemote().sendString("pong")
             })
             res.status(200)
-            res.type("text/event-sourcing")
         })
     }
+
+    private static serveFile(String path, Request req, Response res){
+
+        //InputStream inputStream = getClass().getResourceAsStream(path)
+        def file = new File(path)
+        if (!file.exists()){
+            log.warn("File not found: " + path)
+            return null
+        }
+        InputStream inputStream = new FileInputStream(file)
+
+        if (inputStream != null) {
+            res.status(200)
+
+            byte[] buf = new byte[1024]
+            OutputStream os = res.raw().getOutputStream()
+            OutputStreamWriter outWriter = new OutputStreamWriter(os)
+            int count = 0
+            while ((count = inputStream.read(buf)) >= 0) {
+                os.write(buf, 0, count)
+            }
+            inputStream.close()
+            outWriter.close()
+
+            return ""
+        }
+        return null
+    }
+
 }
